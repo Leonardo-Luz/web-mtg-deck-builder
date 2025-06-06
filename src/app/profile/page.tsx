44d4dac51c8a8028ef8744298c740b015622c11b
Leonardo Luz Fachel <leonardoluz.f02@gmail.com>
2025-06-05 21:24:54 -0300

    feat: update profile route

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,26 +1,52 @@
 'use client'
 
+import axios from "axios";
 import { signOut, useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default () => {
     const { data } = useSession()
 
-    async function logoutHandler() {
-        await signOut()
-        redirect('/login')
+    const [decks, setDecks] = useState(0);
+
+    async function getDecksHandler() {
+        const res = await axios.get(`/api/v1/decks/user/${data!.user.id}`)
+
+        setDecks(res.data.data.length)
     }
+
+    async function logoutHandler() {
+        signOut({ redirect: true, callbackUrl: "/" })
+    }
+
+    useEffect(() => {
+        if (data != null) getDecksHandler();
+    }, [data])
 
     return (
         <div className="mt-50 mb-10 flex flex-col align-middle gap-20">
-            <div className="self-center">
-                <h1 className="font-extrabold text-3xl text-amber-500">{data?.user.username}</h1>
+            <h1 className="self-center font-extrabold text-4xl text-amber-500">PROFILE</h1>
+            <div className="self-center w-[50%] flex flex-col gap-2">
+                <label className="flex flex-row justify-between w-full border-amber-500 border-3 p-2">
+                    <h1 className="font-extrabold text-3xl text-amber-500">Name: </h1>
+                    <h1 className="font-extrabold text-3xl text-amber-500">{data?.user.username || "Loading"}</h1>
+                </label>
+                <label className="flex flex-row justify-between w-full border-amber-500 border-3 p-2">
+                    <h1 className="font-extrabold text-3xl text-amber-500">decks: </h1>
+                    <h1 className="font-extrabold text-3xl text-amber-500">{decks}</h1>
+                </label>
             </div>
 
-            <button
-                className="w-[25%] cursor-pointer self-center rounded-md bg-amber-600 text-black font-extrabold p-3 inset-shadow-sm inset-shadow-[#000000] hover:bg-amber-300"
-                onClick={logoutHandler}
-            >Logout</button>
+            <div className="flex flex-row w-[50%] justify-between align-middle self-center">
+                <button
+                    className="w-[25%] cursor-pointer self-center rounded-md bg-amber-600 text-black font-extrabold p-3 inset-shadow-sm inset-shadow-[#000000] hover:bg-amber-300"
+                    onClick={logoutHandler}
+                >Logout</button>
+                <button
+                    className="w-[25%] cursor-pointer self-center rounded-md bg-amber-600 text-black font-extrabold p-3 inset-shadow-sm inset-shadow-[#000000] hover:bg-amber-300"
+                    onClick={logoutHandler}
+                >Change Password</button>
+            </div>
         </div>
     );
 }
